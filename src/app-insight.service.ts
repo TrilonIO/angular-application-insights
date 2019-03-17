90fb48cee4f5f678215f6573020e59f216163c27
Mark Pieszak <mpieszak84@gmail.com>
2019-03-17 12:35:48 -0400

    publish(7.0.2): publish 7.0.2 to npm
    closes #67
    closes #65

--- src/app-insight.service.ts
+++ src/app-insight.service.ts
@@ -5,6 +5,15 @@ import { filter } from 'rxjs/operators';
 
 import IAppInsights = Microsoft.ApplicationInsights.IAppInsights;
 
+// Since AI.SeverityLevel isn't working we can just use our own
+export enum SeverityLevel {
+  Verbose = 0,
+  Information = 1,
+  Warning = 2,
+  Error = 3,
+  Critical = 4,
+}
+
 export class AppInsightsConfig implements Microsoft.ApplicationInsights.IConfig {
   instrumentationKeySetLater?: boolean;
   // Will be deprecated in next major version
@@ -64,8 +73,12 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#trackevent
-  // trackEvent(name: string, properties?: {[string]:string}, measurements?: {[string]:number})
-  // Log a user action or other occurrence.
+  /**
+   * Log a user action or other occurrence.
+   * @param   name    A string to identify this event in the portal.
+   * @param   properties  map[string, string] - additional data used to filter events and metrics in the portal. Defaults to empty.
+   * @param   measurements    map[string, number] - metrics associated with this event, displayed in Metrics Explorer on the portal. Defaults to empty.
+   */
   trackEvent(eventName: string, eventProperties?: { [name: string]: string }, metricProperty?: { [name: string]: number }) {
     try {
       AppInsights.trackEvent(eventName, eventProperties, metricProperty);
@@ -74,6 +87,10 @@ export class AppInsightsService implements IAppInsights {
     }
   }
 
+  /**
+   * Start timing an extended event. Call {@link stopTrackEvent} to log the event when it ends.
+   * @param   name    A string that identifies this event uniquely within the document.
+   */
   startTrackEvent(name: string): any {
     try {
       AppInsights.startTrackEvent(name);
@@ -82,6 +99,12 @@ export class AppInsightsService implements IAppInsights {
     }
   }
 
+  /**
+   * Log an extended event that you started timing with {@link startTrackEvent}.
+   * @param   name    The string you used to identify this event in startTrackEvent.
+   * @param   properties  map[string, string] - additional data used to filter events and metrics in the portal. Defaults to empty.
+   * @param   measurements    map[string, number] - metrics associated with this event, displayed in Metrics Explorer on the portal. Defaults to empty.
+   */
   stopTrackEvent(name: string, properties?: { [p: string]: string }, measurements?: { [p: string]: number }): any {
     try {
       AppInsights.stopTrackEvent(name, properties, measurements);
@@ -91,8 +114,14 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#trackpageview
-  // trackPageView(name?: string, url?: string, properties?:{[string]:string}, measurements?: {[string]:number}, duration?: number)
-  // Logs that a page or similar container was displayed to the user.
+  /**
+   * Logs that a page or other item was viewed.
+   * @param   name  The string you used as the name in startTrackPage. Defaults to the document title.
+   * @param   url   String - a relative or absolute URL that identifies the page or other item. Defaults to the window location.
+   * @param   properties  map[string, string] - additional data used to filter pages and metrics in the portal. Defaults to empty.
+   * @param   measurements    map[string, number] - metrics associated with this page, displayed in Metrics Explorer on the portal. Defaults to empty.
+   * @param   duration number - the number of milliseconds it took to load the page. Defaults to undefined. If set to default value, page load time is calculated internally.
+   */
   trackPageView(name?: string, url?: string, properties?: { [name: string]: string }, measurements?: { [name: string]: number }, duration?: number) {
     try {
       AppInsights.trackPageView(name, url, properties, measurements, duration);
@@ -102,10 +131,11 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#starttrackpage
-  // startTrackPage(name?: string)
-  // Starts the timer for tracking a page view. Use this instead of trackPageView if you want to control when the
-  // page view timer starts and stops, but don't want to calculate the duration yourself. This method doesn't send any
-  // telemetry. Call stopTrackPage to log the end of the page view and send the event.
+  /**
+   * Starts timing how long the user views a page or other item. Call this when the page opens.
+   * This method doesn't send any telemetry. Call {@link stopTrackTelemetry} to log the page when it closes.
+   * @param   name  A string that idenfities this item, unique within this HTML document. Defaults to the document title.
+   */
   startTrackPage(name?: string) {
     try {
       AppInsights.startTrackPage(name);
@@ -115,9 +145,13 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#stoptrackpage
-  // stopTrackPage(name?: string, url?: string, properties?: Object, measurements?: Object)
-  // Stops the timer that was started by calling startTrackPage and sends the page view telemetry with the
-  // specified properties and measurements. The duration of the page view will be the time between calling startTrackPage and stopTrackPage.
+  /**
+   * Logs how long a page or other item was visible, after {@link startTrackPage}. Call this when the page closes.
+   * @param   name  The string you used as the name in startTrackPage. Defaults to the document title.
+   * @param   url   String - a relative or absolute URL that identifies the page or other item. Defaults to the window location.
+   * @param   properties  map[string, string] - additional data used to filter pages and metrics in the portal. Defaults to empty.
+   * @param   measurements    map[string, number] - metrics associated with this page, displayed in Metrics Explorer on the portal. Defaults to empty.
+   */
   stopTrackPage(name?: string, url?: string, properties?: { [name: string]: string }, measurements?: { [name: string]: number }) {
     try {
       AppInsights.stopTrackPage(name, url, properties, measurements);
@@ -127,9 +161,16 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#trackmetric
-  // trackMetric(name: string, average: number, sampleCount?: number, min?: number, max?: number, properties?: {[string]:string})
-  // Log a positive numeric value that is not associated with a specific event.
-  // Typically used to send regular reports of performance indicators.
+  /**
+   * Log a numeric value that is not associated with a specific event. Typically used to send regular reports of performance indicators.
+   * To send a single measurement, use just the first two parameters. If you take measurements very frequently, you can reduce the
+   * telemetry bandwidth by aggregating multiple measurements and sending the resulting average at intervals.
+   * @param   name    A string that identifies the metric.
+   * @param   average Number representing either a single measurement, or the average of several measurements.
+   * @param   sampleCount The number of measurements represented by the average. Defaults to 1.
+   * @param   min The smallest measurement in the sample. Defaults to the average.
+   * @param   max The largest measurement in the sample. Defaults to the average.
+   */
   trackMetric(name: string, average: number, sampleCount?: number, min?: number, max?: number, properties?: { [name: string]: string }) {
     try {
       AppInsights.trackMetric(name, average, sampleCount, min, max, properties);
@@ -139,10 +180,15 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#trackexception
-  // trackException(exception: Error, handledAt?: string, properties?: {[string]:string}, measurements?: {[string]:number}, severityLevel?: AI.SeverityLevel)
-  // Log an exception you have caught. (Exceptions caught by the browser are also logged.)
+  /**
+   * Log an exception you have caught.
+   * @param   exception   An Error from a catch clause, or the string error message.
+   * @param   properties  map[string, string] - additional data used to filter events and metrics in the portal. Defaults to empty.
+   * @param   measurements    map[string, number] - metrics associated with this event, displayed in Metrics Explorer on the portal. Defaults to empty.
+   * @param   severityLevel   SeverityLevel | AI.SeverityLevel - severity level
+   */
   trackException(exception: Error, handledAt?: string, properties?: { [name: string]: string },
-    measurements?: { [name: string]: number }, severityLevel?: AI.SeverityLevel) {
+    measurements?: { [name: string]: number }, severityLevel?: SeverityLevel | AI.SeverityLevel) {
     try {
       AppInsights.trackException(exception, handledAt, properties, measurements, severityLevel);
     } catch (ex) {
@@ -151,9 +197,14 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#tracktrace
-  // trackTrace(message: string, properties?: {[string]:string}, severityLevel?: AI.SeverityLevel)
+  // trackTrace(message: string, properties?: {[string]:string}, severityLevel?: SeverityLevel | AI.SeverityLevel)
   // Log a diagnostic event such as entering or leaving a method.
-  trackTrace(message: string, properties?: { [name: string]: string }, severityLevel?: AI.SeverityLevel) {
+  /**
+   * Log a diagnostic message.
+   * @param    message A message string
+   * @param    properties  map[string, string] - additional data used to filter traces in the portal. Defaults to empty.
+   */
+  trackTrace(message: string, properties?: { [name: string]: string }, severityLevel?: SeverityLevel | AI.SeverityLevel) {
     try {
       AppInsights.trackTrace(message, properties, severityLevel);
     } catch (ex) {
@@ -162,11 +213,22 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#trackdependency
-  // trackDependency(id: string, method: string, absoluteUrl: string, pathName: string, totalTime: number, success: boolean, resultCode: number)
-  // Log a dependency call (for instance: ajax)
-  trackDependency(id: string, method: string, absoluteUrl: string, pathName: string, totalTime: number, success: boolean, resultCode: number) {
+  /**
+   * Log a dependency call (for instance: ajax)
+   * @param   id    unique id, this is used by the backend o correlate server requests. Use Util.newId() to generate a unique Id.
+   * @param   method    represents request verb (GET, POST, etc.)
+   * @param   absoluteUrl   absolute url used to make the dependency request
+   * @param   pathName  the path part of the absolute url
+   * @param   totalTime total request time
+   * @param   success   indicates if the request was sessessful
+   * @param   resultCode    response code returned by the dependency request
+   * @param   properties    map[string, string] - additional data used to filter events and metrics in the portal. Defaults to empty.
+   * @param   measurements  map[string, number] - metrics associated with this event, displayed in Metrics Explorer on the portal. Defaults to empty.
+   */
+  trackDependency(id: string, method: string, absoluteUrl: string, pathName: string, totalTime: number, success: boolean,
+    resultCode: number, properties?: { [name: string]: string }, measurements?: { [name: string]: number }) {
     try {
-      AppInsights.trackDependency(id, method, absoluteUrl, pathName, totalTime, success, resultCode);
+      AppInsights.trackDependency(id, method, absoluteUrl, pathName, totalTime, success, resultCode, properties, measurements);
     } catch (ex) {
       console.warn('Angular application insights Error [trackDependency]: ', ex);
     }
@@ -186,21 +248,18 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#setauthenticatedusercontext
-  // setAuthenticatedUserContext(authenticatedUserId: string, accountId?: string)
-  // Set the authenticated user id and the account id in this session. Use this when you have identified a specific
-  // signed-in user. Parameters must not contain spaces or ,;=|
-     /**
-     * Sets the authenticated user id and the account id.
-     * User auth id and account id should be of type string. They should not contain commas, semi-colons, equal signs, spaces, or vertical-bars.
-     * 
-     * By default the method will only set the authUserID and accountId for all events in this page view. To add them to all events within
-     * the whole session, you should either call this method on every page view or set `storeInCookie = true`. 
-     *
-     * @param authenticatedUserId {string} - The authenticated user id. A unique and persistent string that represents each authenticated user in the service.
-     * @param accountId {string} - An optional string to represent the account associated with the authenticated user.
-     * @param storeInCookie {boolean} - AuthenticateUserID will be stored in a cookie and added to all events within this session. 
-     */
-  setAuthenticatedUserContext(authenticatedUserId: string, accountId?: string, storeInCookie:boolean = false) {
+  /**
+   * Sets the authenticated user id and the account id.
+   * User auth id and account id should be of type string. They should not contain commas, semi-colons, equal signs, spaces, or vertical-bars.
+   * 
+   * By default the method will only set the authUserID and accountId for all events in this page view. To add them to all events within
+   * the whole session, you should either call this method on every page view or set `storeInCookie = true`. 
+   *
+   * @param authenticatedUserId {string} - The authenticated user id. A unique and persistent string that represents each authenticated user in the service.
+   * @param accountId {string} - An optional string to represent the account associated with the authenticated user.
+   * @param storeInCookie {boolean} - AuthenticateUserID will be stored in a cookie and added to all events within this session. 
+   */
+  setAuthenticatedUserContext(authenticatedUserId: string, accountId?: string, storeInCookie: boolean = false) {
     try {
       AppInsights.setAuthenticatedUserContext(authenticatedUserId, accountId, storeInCookie);
     } catch (ex) {
@@ -209,8 +268,9 @@ export class AppInsightsService implements IAppInsights {
   }
 
   // https://github.com/Microsoft/ApplicationInsights-JS/blob/master/API-reference.md#clearauthenticatedusercontext
-  // clearAuthenticatedUserContext ()
-  // Clears the authenticated user id and the account id from the user context, and clears the associated cookie.
+  /**
+   * Clears the authenticated user id and the account id from the user context.
+   */
   clearAuthenticatedUserContext() {
     try {
       AppInsights.clearAuthenticatedUserContext();
@@ -223,6 +283,10 @@ export class AppInsightsService implements IAppInsights {
     console.warn('Angular application insights Error [_onerror]: ', message);
   }
 
+  /**
+   * Initialize Application Insights for Angular
+   * Make sure your config{} has been set
+   */
   public init(): void {
     if (this.config) {
 
@@ -283,4 +347,3 @@ export class AppInsightsService implements IAppInsights {
     }
   }
 }
-
